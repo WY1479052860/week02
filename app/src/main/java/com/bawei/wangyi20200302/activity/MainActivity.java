ddc884456eccc2cf36576d34fe289a86b6c32e63
WY1479052860 <1479052860@qq.com>
2020-03-02 13:06:42 +0800

    这是第二周周考的项目

---
+++ app/src/main/java/com/bawei/wangyi20200302/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bawei.wangyi20200302.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bawei.wangyi20200302.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
